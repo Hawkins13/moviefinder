e8308d75bf10bbb9eab87d5eec4e777ff6dfad44
Lee Hawkins <Lee Hawkins>
2016-02-21 13:59:58 -0600

    Initial commit

---
+++ src/main/java/com/moviefinder/MoviefinderApplication.java
@@ -0,0 +1,12 @@
+package com.moviefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviefinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviefinderApplication.class, args);
+	}
+}
